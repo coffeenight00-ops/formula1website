de3e03b1b1417b93281c4875c1702068e42ca139
Anas <shwekianas8@gmail.com>
2025-11-28 06:06:40 -0800

    fix vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/formula1website/", // VERY IMPORTANT FOR GITHUB PAGES
+  base: "./",   // <-- IMPORTANT FOR GITHUB PAGES
 });
